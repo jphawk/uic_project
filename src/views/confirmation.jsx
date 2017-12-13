fa5ce2e355fc842cdc4c1e84147eec2c1796182c
Elena Kazakova <elena.kazakova@aalto.fi>
2017-12-13 14:22:09 +0300

    Confirmation screen is fixed

--- src/views/confirmation.jsx
+++ src/views/confirmation.jsx
@@ -16,17 +16,21 @@ export default class ConfirmationPage extends Component {
 					<meta name="confirmation" content="Confirmation page" />
 					<meta name="author" content="Team Translator" />
 				</Helmet>
+                
+                <Header />
 
-				<div id="page-confirmation" class="page">
+				<div id="page-confirmation" className="page">
 					<div id="content" className="confirmation">
 						<h1>Thank you for editing this translation!</h1>
-						<h3>Is this the final version?</h3>
+                        <h3>Your work was successfully saved</h3>
+						<h4>Is this your final version?</h4>
 						<div id="confirmation-buttons-wrapper">
 							<a className="dark-link" href={"/translator/" + this.props.match.params.translationid} title="Yes, submit">Yes, submit</a>
-							<a className="light-link" href={'/translation/' + this.props.match.params.translationid + '/edit'} title="No, back to Editor">No, back to Editor</a>
+							<a className="light-link" href="/translator" title="Back to the Dashboard">Back to the Dashboard</a>
 						</div>
 						<div id="bottom-link-confirmation">
-							<a class="dark-link confirmation" href="/translator" title="Back to the Dashboard">Back to the Dashboard</a>
+				            <h4>Want to continue editing your translation?</h4>
+                            <a className="light-link" href={'/translation/' + this.props.match.params.translationid + '/edit'} title="Back to Editor">Back to Editor</a>
 						</div>
 					</div>
 				</div>
